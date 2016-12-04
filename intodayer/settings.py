61d7c41b9764c89cb4f5609435b6fee337386639
Alexey Krotkov <alexeykrotkov@yahoo.com>
2016-12-04 21:37:31 +0400

    new design yea

--- intodayer/settings.py
+++ intodayer/settings.py
@@ -15,7 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-STATICFILES_DIRS = ('C:/djangoProjects/intodayer/static',)
+STATICFILES_DIRS = ('F:\My documents\Programming\GitHub\intodayer\static',)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'intodayer.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['C:/djangoProjects/intodayer/templates'],
+        'DIRS': [r'F:\My documents\Programming\GitHub\intodayer\templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- static/scripts/script_1.js
+++ static/scripts/script_1.js
@@ -6,21 +6,20 @@
 
 // Определяем на какой мы странице
 var locate = location.toString();
-var ind = locate.lastIndexOf('/');
-var nowPage = null;
-if (ind >= 0) {
-    nowPage = locate.slice(ind + 1, locate.length - 5);
-    setCurrentMenu(nowPage);
+var home = locate.lastIndexOf('home');
+
+if (home >= 0) {
+    setCurrentMenu('table');
 }
 
 function setCurrentMenu(nameClass) {
     var menu_div = document.getElementsByClassName('menu');
     var menu_elements = menu_div[0].getElementsByTagName('li');
     for (var i = 0; i < menu_elements.length; i++) {
-        menu_elements[i].style.width = '20%';
+        menu_elements[i].style.width = '100%';
     }
     var setCurrent = menu_div[0].getElementsByClassName(nameClass);
-    setCurrent[0].style.width = '100%';
+    // setCurrent[0].style.width = '100%';
     setCurrent[0].style.boxShadow = 'none';
     setCurrent[0].style.backgroundColor = 'white';
 //	setCurrent[0].style.borderLeft = '3px solid #8b1deb';
