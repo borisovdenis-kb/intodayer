f46b11969f28d9e13ccb339f341a95cdee3389b5
Alexey Krotkov <alexeykrotkov@yahoo.com>
2016-12-08 21:42:30 +0400

    super-puper
    )))

--- test/scripts/script_1.js
+++ test/scripts/script_1.js
@@ -1,58 +1,191 @@
-// Определяем на какой мы странице
-var locate = location.toString();
-var home = locate.lastIndexOf('home');
+// устанавливает подстветку меню
+//var locate = location.toString();
+//var home = locate.lastIndexOf('home');
 
-if (home >= 0) {
-	setCurrentMenu('group');
-}
-setCurrentMenu('group');
-function setCurrentMenu(nameClass) {
+var selectedMenu;
+var selectedDay;
+
+//Меню
+selectedMenu = document.getElementById("home");
+setCurrentMenu(selectedMenu);
+
+//Дни недели
+selectedDay = document.querySelectorAll('.days')[0];
+selectedDay.classList.add('days_select');
+
+function setCurrentMenu(objElem) {
 	"use strict";
-	var menu_div = document.getElementsByClassName('menu');
-	var menu_elements = menu_div[0].getElementsByTagName('li');
-	for (var i = 0; i < menu_elements.length; i++) {
-		menu_elements[i].style.width = '100%';
+	objElem.classList.add('select_menu');
+	var prev = objElem.previousSibling.previousSibling;
+	var next = objElem.nextSibling.nextSibling;
+	if (prev !== null && prev.tagName === 'LI') {
+		prev.classList.add('leftShadow');
 	}
-	var setCurrent = menu_div[0].getElementsByClassName(nameClass);
-	setCurrent[0].style.boxShadow = 'none';
-	setCurrent[0].style.backgroundColor = 'white';
-	var right_shadow;
-	var left_shadow;
-	if (nameClass === 'table') {
-		right_shadow = menu_div[0].getElementsByClassName('home');
-		right_shadow[0].style.boxShadow = 'inset -15px 0px 36px -19px rgba(0,0,0,0.5)';
 
+	if (next !== null && next.tagName === 'LI') {
+		next.classList.add('rightShadow');
 	}
-	if (nameClass === 'events') {
-		right_shadow = menu_div[0].getElementsByClassName('table');
-		right_shadow[0].style.boxShadow = 'inset -15px 0px 36px -19px rgba(0,0,0,0.5)';
-	}
-	if (nameClass === 'group') {
-		right_shadow = menu_div[0].getElementsByClassName('events');
-		left_shadow = menu_div[0].getElementsByClassName('about');
-		right_shadow[0].style.boxShadow = 'inset -15px 0px 36px -19px rgba(0,0,0,0.5)';
-		left_shadow[0].style.boxShadow = 'inset 15px 0px 36px -19px rgba(0,0,0,0.5)';
-	}
-	if (nameClass === 'about') {
-		right_shadow = menu_div[0].getElementsByClassName('group');
-		right_shadow[0].style.boxShadow = 'inset -15px 0px 36px -19px rgba(0,0,0,0.5)';
-	}
+	selectedMenu = objElem;
 }
 
-//выпадающее меню
-function DropDown(el) {
+//Обработка нажатий на меню
+function setDefaultMenu() {
 	"use strict";
-	this.dd = el;
-	this.initEvents();
+	var next = selectedMenu.nextSibling.nextSibling;
+	var prev = selectedMenu.previousSibling.previousSibling;
+	selectedMenu.classList.remove('select_menu');
+	if (next && next.tagName === 'LI') {
+		if (next.classList.contains('leftShadow')) {
+			next.classList.remove('leftShadow');
+		}
+		if (next.classList.contains('rightShadow')) {
+			next.classList.remove('rightShadow');
+		}
+	}
+	if (prev && prev.tagName === 'LI') {
+		if (prev.classList.contains('leftShadow')) {
+			prev.classList.remove('leftShadow');
+		}
+		if (prev.classList.contains('rightShadow')) {
+			prev.classList.remove('rightShadow');
+		}
+	}
 }
-DropDown.prototype = {
-	initEvents: function () {
+
+function setMenu() {
+	"use strict";
+	var objElem = event.target.parentElement;
+	setDefaultMenu(objElem);
+	setCurrentMenu(objElem);
+}
+
+var menuElem = document.getElementById("menu");
+menuElem.addEventListener('click', setMenu);
+
+//Отключает работу ссылок <a></a> в выборе дней
+var elemsA = document.querySelectorAll('.days');
+for (var i = 0; i < elemsA.length; i++) {
+	elemsA[i].onclick = function () {
 		"use strict";
-		var obj = this;
+		return false;
+	};
+}
 
-		obj.dd.on('click', function (event) {
-			$(this).toggleClass('active');
-			event.stopPropagation();
-		});
+//Отключает работу ссылок <a></a> в главном меню для тестового режима
+//===============================================
+var elemsMenu = document.getElementById("menu");
+elemsMenu = elemsMenu.getElementsByTagName("LI");
+for (var i = 0; i < elemsMenu.length; i++) {
+	elemsMenu[i].onclick = function () {
+		"use strict";
+		return false;
+	};
+}
+//===============================================
+
+//Обратотка нажаний на вывод таблиц
+var daysElem = document.getElementsByClassName("week")[0];
+daysElem.addEventListener('click', setDay);
+
+function setDay() {
+	"use strict";
+	var objElem = event.target.parentElement;
+	var prevSelect = selectedDay;
+
+	if (objElem && objElem.tagName === "LI") {
+		if (prevSelect.classList.contains('days_select')) {
+			prevSelect.classList.remove('days_select');
+		}
+		objElem.classList.add('days_select');
+		selectedDay = objElem;
+		
+//		тут нужно изменить саму таблицу
+		
+		
+		
+		
+		
+		
+//			
+				
+		$('tr').clearQueue();
+		hideTable(40,400);
+		showTable(40,400);
 	}
-};
+}
+
+//Открытие и закрытие меню профиля в шапке
+var droplink = document.getElementsByClassName('droplink')[0];
+var ul_menu = droplink.getElementsByTagName("UL")[0];
+var arrow = document.getElementsByClassName("up-arrow")[0];
+var openFlag = false;
+var timeMenuEffect = 150;
+
+droplink.addEventListener("click", openMenu);
+document.body.addEventListener("click", closeMenu);
+
+function openMenu() {
+	"use strict";
+	if (openFlag === false) {
+		arrow.classList.add('select_up-arrow');
+		$(ul_menu).animate({
+			'margin-top': '10px'
+		}, timeMenuEffect);
+		$('.dropmenu').fadeIn(timeMenuEffect);
+		openFlag = true;
+	} else if (!ul_menu.contains(event.target)) {
+		closeDroplist();
+	}
+}
+
+function closeMenu() {
+	"use strict";
+	if (openFlag && !droplink.contains(event.target)) {
+		closeDroplist();
+	}
+}
+
+function closeDroplist() { //устанавливает свойста для закрытия droplist
+	"use strict";
+	openFlag = false;
+	$('.dropmenu').fadeOut(timeMenuEffect);
+	$(ul_menu).animate({
+		'margin-top': '20px'
+	}, timeMenuEffect);
+	if (arrow.classList.contains("select_up-arrow")) {
+		arrow.classList.remove("select_up-arrow");
+	}
+}
+
+//появление таблицы
+var heightTable = $('.table-container').height();
+$('.table-container').css({'height':heightTable});
+$('tr').hide();
+
+
+$(document).ready ( function(){
+	"use strict";
+	showTable(40,400);
+});
+
+function showTable(interval, time){
+	"use strict";
+	$('tr').each(function(i) {
+		$(this).delay((i++) * interval).fadeTo(time,1);
+	});	
+}
+
+function hideTable(interval, time){
+	"use strict";
+	$('tr').each(function(i) {
+		$(this).delay((i++) * interval).fadeTo(time,0);
+	});	
+}
+
+
+
+
+
+
+
+
