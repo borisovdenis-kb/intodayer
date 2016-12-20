7b44bf5b1f7f66a3564cdfeb4e9e4f0cfdac4d58
nostromo <borisovdenis-kb@yandex.ru>
2016-12-20 17:30:35 +0300

    О

--- intodayer2_app/views.py
+++ intodayer2_app/views.py
@@ -135,7 +135,45 @@ def add_schedules_view(request):
     сообщения, "Создайте расписание".
     """
     if request.user.is_authenticated():
+        user = User.objects.get(username=request.user.username)
+        try:
+            std_id = user.customuser.stdt_stdt.stdt_id
+        except ObjectDoesNotExist:
+            context = {'table' : [],
+                       'username' : user.username
+            }
+        else:
+            student = Students.objects.get(stdt_id = std_id)
+            group = student.grp_grp_id
+            cathedra = student.cthd_cthd_id
 
+            # выбираем из тиблицы расписания все записи, "нужные" данному юзеру
+            # мы получили список, состоящий из строк расписания
+            # далее генерируем расписание на неделю, в зависимости от номера и четности недели
+            table = list(Schedules.objects.filter(grp_grp_id = group, cthd_cthd_id = cathedra))
+
+            current_week = [[], [], [], [], [], [], []]
+
+            for row in table:
+                if row.dfwk_dfwk.name == 'Понедельник':
+                    current_week[0].append(row)
+                elif row.dfwk_dfwk.name == 'Вторник':
+                    current_week[1].append(row)
+                elif row.dfwk_dfwk.name == 'Среда':
+                    current_week[2].append(row)
+                elif row.dfwk_dfwk.name == 'Четверг':
+                    current_week[3].append(row)
+                elif row.ddfwk_dfwk.name == 'Пятница':
+                    current_week[4].append(row)
+                elif row.dfwk_dfwk.name == 'Суббота':
+                    current_week[5].append(row)
+                elif row.dfwk_dfwk.name == 'Воскресенье':
+                    current_week[6].append(row)
+
+            context = {'table' : current_week,
+                       'username' : user.username
+            }
+            return render_to_response('add_schedules.html', context)
     else:
         return HttpResponseRedirect('/login')
 
