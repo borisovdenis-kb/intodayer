287402e6e107b043f41356cc9cad690376ab8ab8
borisovdenis-kb <borisovdenis-kb@yandex.ru>
2017-01-18 21:10:10 +0400

    Проверка на совпадение пароля №1 и пароля №2

--- static/scripts/script_reg_info_check.js
+++ static/scripts/script_reg_info_check.js
@@ -1,24 +1,34 @@
-// валидация пароля
+
 
 $(document).ready( function(){
+    // валидация пароля
     $("#password1").keyup( function(){
         var content, secure_pattern;
 
         secure_pattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])|(?=.*\d)(?=.*[а-я])(?=.*[А-Я])(?!.*\s).*$/;
         
-        content = $("#password1").val();
+        content = $('#password1').val();
         
         $('#pswdContent').text(content);
 
         if (content.length >= 8) {
             if (secure_pattern.test(content) == true){
-                $('#circle_indicator1').css("background-color", '#0DA559');
+                $('#circle_indicator1').css('background-color', '#0DA559');
             } else {
-                $('#circle_indicator1').css("background-color", '#FFD600');
+                $('#circle_indicator1').css('background-color', '#FFD600');
             }
         } else {
-            $('#circle_indicator1').css("background-color", '#FF4233');
+            $('#circle_indicator1').css('background-color', '#FF4233');
         }
-        
+    });
+    // проверка на сопадение пароля1 и пароля2
+    $("#password2").keyup( function() {
+        if ($('#password1').val() == $('#password2').val()){
+            $('#password1').css('background-color', 'FFED94');
+            $('#password2').css('background-color', 'FFED94');
+        } else {
+            $('#password1').css('background-color', 'FFFFFF');
+            $('#password2').css('background-color', 'FFFFFF');
+        }
     });
 });
