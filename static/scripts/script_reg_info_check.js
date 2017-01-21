5d09a1cbddb0735e4ddb07773017b97d33bcdd22
borisovdenis-kb <borisovdenis-kb@yandex.ru>
2017-01-21 23:05:44 +0400

    переделал анимацию в плане алгоритма

--- static/scripts/script_reg_info_check.js
+++ static/scripts/script_reg_info_check.js
@@ -1,8 +1,8 @@
-var width_size, raise, is_correct, old_value;
+var width_size, raise, is_correct, old_content;
 raise = 0;
 width_size = 0;
 is_correct = 0;
-old_value = $('#password1').val();
+old_content = $('#password1').val();
 
 
 $(document).ready( function(){
@@ -10,45 +10,72 @@ $(document).ready( function(){
         Валидация пароля
     */
     $("#password1").focus( function(){
-
         $('#circle_indicator1').animate({opacity: '0.8'}, 1);
-        $("#password1").keyup( function(event){
-            if ($('#password1').val() != old_value) {
-                var content, secure_pattern;
-                secure_pattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])|(?=.*\d)(?=.*[а-я])(?=.*[А-Я])(?!.*\s).*$/;
-                content = $('#password1').val();
 
-                if (event.keyCode == 8) { // нажата backspace
+        $("#password1").keyup( function(event){
+            var content, secure_pattern, len;
+            secure_pattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])|(?=.*\d)(?=.*[а-я])(?=.*[А-Я])(?!.*\s).*$/;
+            content = $('#password1').val();
+            len = content.length;
+
+            if (content != old_content) {
+                if (event.keyCode == 8) {
+                    if (width_size >= 299 && is_correct == 1) {
+                        width_size -= raise;
+                    }
+                    for(var i=0; i < Math.abs(len - old_content.length); i++){
+                        if (width_size <= 144){
+                            width_size -= 18;
+                        } else if (width_size > 144 && width_size <= 216){
+                            width_size -= 9;
+                        } else if (width_size > 216 && width_size < 300){
+                            width_size -= 3;
+                        }
+                    }
+                } else {
+                    for(var i=0; i < Math.abs(len - old_content.length); i++){
+                        if (width_size < 144){
+                            width_size += 18;
+                        } else if (width_size >= 144 && width_size < 216){
+                            width_size += 9;
+                        } else if (width_size >= 216 && width_size < 300){
+                            width_size += 3;
+                        }
+                    }
+                }
+
+                /*if (event.keyCode == 8) { // нажата backspace
                     if (width_size >= 299 && is_correct == 1) {
                         width_size -= raise;
                     }
 
                     if (content.length >= 16) {
                         width_size -= 3;
-                        $('#circle_indicator1').animate({width: width_size + 'px'}, 40);
+                        //$('#circle_indicator1').animate({width: width_size + 'px'}, 40);
                     } else if (content.length >= 8 && content.length < 16) {
                         width_size -= 9;
-                        $('#circle_indicator1').animate({width: width_size + 'px'}, 40);
+                        //$('#circle_indicator1').animate({width: width_size + 'px'}, 40);
                     } else if (content.length <= 8 && width_size > 0) {
                         width_size -= 18;
-                        $('#circle_indicator1').animate({width: width_size + 'px'}, 40);
+                        //$('#circle_indicator1').animate({width: width_size + 'px'}, 40);
                     }
                 } else { // нажата любая другая клавиша
                     if (width_size >= 300) {
-                        $('#circle_indicator1').animate({width: width_size + 'px'}, 40);
+                        //$('#circle_indicator1').animate({width: width_size + 'px'}, 40);
                     } else if (content.length >= 16) {
                         width_size += 3;
-                        $('#circle_indicator1').animate({width: width_size + 'px'}, 40);
+                        //$('#circle_indicator1').animate({width: width_size + 'px'}, 40);
                     } else if (content.length > 8 && content.length < 16) {
                         width_size += 9;
-                        $('#circle_indicator1').animate({width: width_size + 'px'}, 40);
+                        //$('#circle_indicator1').animate({width: width_size + 'px'}, 40);
                     } else {
                         width_size += 18;
-                        $('#circle_indicator1').animate({width: width_size + 'px'}, 40);
+                        //$('#circle_indicator1').animate({width: width_size + 'px'}, 40);
                     }
-                }
+                }*/
+                $('#circle_indicator1').animate({width: width_size + 'px'}, 40);
 
-                $('#pswdContent').text(content + '|' + width_size + '|' + raise + '|' + old_value);
+                $('#pswdContent').text(content + '|' + width_size + '|' + raise + '|' + old_content);
 
                 if (content.length >= 8) {
                     if (secure_pattern.test(content) == true) {
@@ -64,7 +91,7 @@ $(document).ready( function(){
                     $('#circle_indicator1').animate({backgroundColor: '#FF4233'}, 40);
                 }
 
-                old_value = $('#password1').val();
+                old_content = $('#password1').val();
             }
         });
     });
