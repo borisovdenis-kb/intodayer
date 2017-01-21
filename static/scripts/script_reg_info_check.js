4f24a242b4b257bd4a31b3388fa5276786d5c913
borisovdenis-kb <borisovdenis-kb@yandex.ru>
2017-01-21 22:03:08 +0400

    Доделываю анимацию валидации пароля

--- static/scripts/script_reg_info_check.js
+++ static/scripts/script_reg_info_check.js
@@ -1,7 +1,8 @@
-var width_size, raise, is_correct;
+var width_size, raise, is_correct, old_value;
 raise = 0;
 width_size = 0;
 is_correct = 0;
+old_value = $('#password1').val();
 
 
 $(document).ready( function(){
@@ -10,61 +11,60 @@ $(document).ready( function(){
     */
     $("#password1").focus( function(){
 
-
         $('#circle_indicator1').animate({opacity: '0.8'}, 1);
         $("#password1").keyup( function(event){
-            var content, secure_pattern;
-            secure_pattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])|(?=.*\d)(?=.*[а-я])(?=.*[А-Я])(?!.*\s).*$/;
-            content = $('#password1').val();
+            if ($('#password1').val() != old_value) {
+                var content, secure_pattern;
+                secure_pattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])|(?=.*\d)(?=.*[а-я])(?=.*[А-Я])(?!.*\s).*$/;
+                content = $('#password1').val();
 
-            if (event.keyCode == 8){ // нажата backspace
-                if (width_size >= 299 && is_correct == 1){
-                    width_size -= raise;
-                    $('#circle_indicator1').animate({width: width_size + 'px'}, 40);
-                } else if (content.length >= 16){
-                    width_size -= 3;
-                    $('#circle_indicator1').animate({width: width_size + 'px'}, 40);
-                } else if (content.length >= 8 && content.length < 16) {
-                    width_size -= 9;
-                    $('#circle_indicator1').animate({width: width_size + 'px'}, 40);
-                } else if (content.length <= 8 && width_size > 0) {
-                    width_size -= 18;
-                    $('#circle_indicator1').animate({width: width_size + 'px'}, 40);
+                if (event.keyCode == 8) { // нажата backspace
+                    if (width_size >= 299 && is_correct == 1) {
+                        width_size -= raise;
+                    }
+
+                    if (content.length >= 16) {
+                        width_size -= 3;
+                        $('#circle_indicator1').animate({width: width_size + 'px'}, 40);
+                    } else if (content.length >= 8 && content.length < 16) {
+                        width_size -= 9;
+                        $('#circle_indicator1').animate({width: width_size + 'px'}, 40);
+                    } else if (content.length <= 8 && width_size > 0) {
+                        width_size -= 18;
+                        $('#circle_indicator1').animate({width: width_size + 'px'}, 40);
+                    }
+                } else { // нажата любая другая клавиша
+                    if (width_size >= 300) {
+                        $('#circle_indicator1').animate({width: width_size + 'px'}, 40);
+                    } else if (content.length >= 16) {
+                        width_size += 3;
+                        $('#circle_indicator1').animate({width: width_size + 'px'}, 40);
+                    } else if (content.length > 8 && content.length < 16) {
+                        width_size += 9;
+                        $('#circle_indicator1').animate({width: width_size + 'px'}, 40);
+                    } else {
+                        width_size += 18;
+                        $('#circle_indicator1').animate({width: width_size + 'px'}, 40);
+                    }
                 }
-            } else { // нажата любая другая клавиша
-                if (width_size >= 300) {
-                    $('#circle_indicator1').animate({width: width_size + 'px'}, 40);
-                } else if (content.length >= 16){
-                    width_size += 3;
-                    $('#circle_indicator1').animate({width: width_size + 'px'}, 40);
-                } else if (content.length > 8 && content.length < 16){
-                    width_size += 9;
-                    $('#circle_indicator1').animate({width: width_size + 'px'}, 40);
+
+                $('#pswdContent').text(content + '|' + width_size + '|' + raise + '|' + old_value);
+
+                if (content.length >= 8) {
+                    if (secure_pattern.test(content) == true) {
+                        raise = 299 - width_size;
+                        width_size = 299;
+                        is_correct = 1;
+                        $('#circle_indicator1').animate({backgroundColor: '#0DA559'}, 40);
+                        $('#circle_indicator1').animate({width: width_size + 'px'}, 80);
+                    } else {
+                        $('#circle_indicator1').animate({backgroundColor: '#FFD600'}, 40);
+                    }
                 } else {
-                    width_size += 18;
-                    $('#circle_indicator1').animate({width: width_size + 'px'}, 40);
+                    $('#circle_indicator1').animate({backgroundColor: '#FF4233'}, 40);
                 }
-            }
 
-            $('#pswdContent').text(content + '|' + width_size + '|' + raise);
-
-            // $('#circle_indicator1').animate({width: '10px', height: '38px'}, 40);
-
-            if (content.length >= 8) {
-                if (secure_pattern.test(content) == true){
-                    // $('#circle_indicator1').css('background-color', '#0DA559');
-                    //raise = 299 - width_size;
-                    width_size = 299;
-                    is_correct = 1;
-                    $('#circle_indicator1').animate({backgroundColor: '#0DA559'}, 40);
-                    $('#circle_indicator1').animate({width: width_size + 'px'}, 80);
-                } else {
-                    // $('#circle_indicator1').css('background-color', '#FFD600');
-                    $('#circle_indicator1').animate({backgroundColor: '#FFD600'}, 40);
-                }
-            } else {
-                // $('#circle_indicator1').css('background-color', '#FF4233');
-                $('#circle_indicator1').animate({backgroundColor: '#FF4233'}, 40);
+                old_value = $('#password1').val();
             }
         });
     });
