3bf513e03a158f97a148431fac88305009867970
borisovdenis-kb <borisovdenis-kb@yandex.ru>
2017-01-30 16:47:38 +0400

    Леха мне помог сделать нормальный JS

--- static/scripts/script_reg_info_check.js
+++ static/scripts/script_reg_info_check.js
@@ -1,4 +1,6 @@
 var width_size, raise, is_correct, old_content;
+var input_width = $('.reg input').outerWidth();
+var factor = input_width / 10;
 raise = 0;
 width_size = 0;
 is_correct = 0;
@@ -22,54 +24,59 @@ $(document).ready( function(){
             - весь алгоритм рпивязан к конкретным значениям. он
               не универсален
             */
-            if (content != old_content) {
-                if (event.keyCode == 8) {
-                    if (width_size >= 299 && is_correct == 1) {
-                        width_size -= raise;
-                    }
-                    for(var i=0; i < Math.abs(len - old_content.length); i++){
-                        if (width_size <= 144){
-                            width_size -= 18;
-                        } else if (width_size > 144 && width_size <= 216){
-                            width_size -= 9;
-                        } else if (width_size > 216 && width_size < 300){
-                            width_size -= 3;
-                        }
-                    }
-                } else {
-                    for (var i = 0; i < Math.abs(len - old_content.length); i++) {
-                        if (width_size < 144) {
-                            width_size += 18;
-                        } else if (width_size >= 144 && width_size < 216) {
-                            width_size += 9;
-                        } else if (width_size >= 216 && width_size < 300) {
-                            width_size += 3;
-                        }
-                    }
-                }
+            // if (content != old_content) {
+            //     if (event.keyCode == 8) { // если backspace
+            //         if (width_size >= 299 && is_correct == 1) {
+            //             width_size -= raise;
+            //         }
+            //         for(var i=0; i < Math.abs(len - old_content.length); i++){
+            //             if (width_size <= 144){
+            //                 width_size -= 18;
+            //             } else if (width_size > 144 && width_size <= 216){
+            //                 width_size -= 9;
+            //             } else if (width_size > 216 && width_size < 300){
+            //                 width_size -= 3;
+            //             }
+            //         }
+            //     } else {
+            //         for (var i = 0; i < Math.abs(len - old_content.length); i++) {
+            //             if (width_size < 144) {
+            //                 width_size += 18;
+            //             } else if (width_size >= 144 && width_size < 216) {
+            //                 width_size += 9;
+            //             } else if (width_size >= 216 && width_size < 300) {
+            //                 width_size += 3;
+            //             }
+            //         }
+            //     }
+            //
+            //     if (!is_correct) {
+            //         $('#circle_indicator1').animate({width: width_size + 'px'}, 40);
+            //     }
 
-                if (!is_correct) {
-                    $('#circle_indicator1').animate({width: width_size + '%'}, 40);
-                }
+            if (secure_pattern.test(content) == false) {
+                width_size = len * factor / (1.1 + len/10);
+                $('#circle_indicator1').animate({width: width_size + 'px'}, 40);
+                $('#circle_indicator1').clearQueue();
+            }
 
-                if (content.length >= 8) {
-                    if (secure_pattern.test(content) == true) {
-                        raise = 299 - width_size;
-                        width_size = 299;
-                        is_correct = 1;
-                        $('#circle_indicator1').animate({backgroundColor: '#0DA559'}, 40);
-                        $('#circle_indicator1').animate({width: width_size + 'px'}, 80);
-                    } else {
-                        is_correct = 0;
-                        $('#circle_indicator1').animate({backgroundColor: '#FFD600'}, 40);
-                    }
+            if (content.length >= 8) {
+                if (secure_pattern.test(content) == true) {
+                    // raise = 299 - width_size;
+                    // width_size = 299;
+                    // is_correct = 1;
+                    $('#circle_indicator1').animate({backgroundColor: '#0DA559'}, 40);
+                    $('#circle_indicator1').animate({width: input_width-1 + 'px'}, 80);
                 } else {
                     is_correct = 0;
-                    $('#circle_indicator1').animate({backgroundColor: '#FF4233'}, 40);
+                    $('#circle_indicator1').animate({backgroundColor: '#FFD600'}, 40);
                 }
-
-                old_content = $('#password1').val();
+            } else {
+                is_correct = 0;
+                $('#circle_indicator1').animate({backgroundColor: '#FF4233'}, 40);
             }
+
+            old_content = $('#password1').val();
         });
     });
 
