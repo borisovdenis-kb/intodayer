252274eec6477869fab2b03ca0d19cc336ac7fd8
borisovdenis-kb <borisovdenis-kb@yandex.ru>
2017-02-02 16:36:18 +0400

    Обезапасили базу! (reverted from commit 7ec57c6bd937cf55638d65909a78481657190b61)

--- intodayer2/settings.py
+++ intodayer2/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
-from intodayer2.config import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -78,12 +77,14 @@ WSGI_APPLICATION = 'intodayer2.wsgi.application'
 
 DATABASES = {
     'default': {
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE': 'django.db.backends.oracle',
-        'NAME': db_config['NAME'],
-        'USER': db_config['USER'],
-        'PASSWORD': db_config['PASSWORD'],
-        'HOST': db_config['HOST'],
-        'PORT': db_config['PORT'],
+        'NAME': 'ORCL',
+        'USER': 'intodayer_tst',
+        'PASSWORD': 'tst',
+        'HOST': 'ctfssu.ru',
+        'PORT': '1521',
     }
 }
 
