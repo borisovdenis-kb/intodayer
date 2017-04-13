a45027028e03ff3039d18add7250b27cd6357699
Denis Borisov <Denis Borisov>
2017-04-14 00:47:20 +0300

    Пофиксил косяки в работе рассылки: теперь картинка отправляется так как нужно, и реализована фича как в контакте, когда ввел ссобщение не отправил его, вышел из диалога, потом опять заходишь, а введенное сообщение никуда не пропало!

--- static/scripts/script_mailing.js
+++ static/scripts/script_mailing.js
@@ -1,17 +1,39 @@
 
-
-// удалить все клоссы check_box_wrap
-// у всех блоков с классом weeks border left прозрачный
-
-var imageData, timer;
+var imageData, timer, day, storageKey;
 
 $(document).ready(function () {
 
-    $('.share_button').click(function () {
-        blurElement('.effect_blur', 4);
-        $('.cover_all').fadeIn(800);
-        $('.mailing_wrap').delay(300).fadeIn(500);
-        $('.mailing').slideToggle(800, 'easeInOutBack').css({'display': 'flex'});
+    $('.share_button').each(function () {
+        $(this).click(function () {
+
+            var $textarea = $('.mailing').find('textarea');
+            day = $(this).parent().parent().parent().attr('day_num');
+            storageKey = day + $('.title_content').attr('plan_id');
+
+            blurElement('.effect_blur', 4);
+            $('.cover_all').fadeIn(800);
+            $('.mailing_wrap').delay(300).fadeIn(500);
+            $('.mailing').slideToggle(800, 'easeInOutBack').css({'display': 'flex'});
+
+            if ('key' in localStorage) {
+                $textarea.val(localStorage[storageKey]);
+            }
+
+            $textarea.off();
+            $textarea.on('input', function () {
+                var thisTextArea = $(this);
+
+                localStorage.setItem(storageKey, thisTextArea.val());
+
+                var timerInputId = setInterval(function () {
+                    validateTextArea(thisTextArea);
+                }, 50);
+
+                setTimeout(function () {
+                    clearInterval(timerInputId);
+                }, 500);
+            });
+        });
     });
 
     $('.mailing_close').click(function () {
@@ -24,17 +46,29 @@ $(document).ready(function () {
 
     $('.do_mailing_button').click(function() {
 
-        var $elem = $(this).parent().parent().parent();
-        var $clone = $elem.clone(); // plan_content
+        var $elem, $clone;
         var $KLOSS = $('.KLOSS');
         var $textarea = $(this).parent().parent().find('textarea');
         var data = {};
 
+        $.each($('.plan_window'), function() {
+            if ($(this).attr('day_num') == day){
+                $elem = $(this);
+            }
+        });
+
+        $clone = $elem.clone();
+
+        // убираем лишние элементы
         $.each($clone.find('.checkbox_wrap'), function () {
             $(this).remove();
         });
 
-        // $KLOSS.css({'top': '10000px'});
+        $clone.find('.delete_row').remove();
+        $clone.find('.clone_row').remove();
+        $clone.find('.share_button').remove();
+        $clone.find('.plus_button').remove();
+
         $KLOSS.append($clone);
 
         htmlToImage($clone);
@@ -58,6 +92,7 @@ $(document).ready(function () {
                         msg = JSON.parse(msg);
                         $('.progress').css({'display': 'none'});
                         $('.mailing_close').trigger('click');
+                        localStorage.setItem(storageKey, '');
                         $textarea.val('');
                     }
                 });
@@ -69,17 +104,26 @@ $(document).ready(function () {
 });
 
 
+function validateTextArea(textArea) {
+    /*
+     *  Функция валидации поля ввода сообщения.
+     *  Ограничение на длинну сообщения: 1000 символов.
+     */
+    if (textArea.val().length > 1000) {
+        textArea.val(textArea.val().slice(0, 1000));
+    }
+}
+
+
 function htmlToImage($elem) {
     /*
      *  Функция конвертирует блок HTML в изображение
      *  И отправляет на сервер.
      */
-
     html2canvas($elem, {
         onrendered: function (canvas) {
             imageData = canvas.toDataURL("image/png");
-
-            $("#btn-Convert-Html2Image").attr("download", "your_pic_name.png").attr("href", imageData);
+            // $("#btn-Convert-Html2Image").attr("download", "your_pic_name.png").attr("href", imageData);
         }
     });
 }
