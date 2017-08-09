65c2b5b4b93939ebd59aaadcf02b838edf44ea03
Denis <borisovdenis-kb@yandex.ru>
2017-08-09 08:26:06 +0400

    Добавил подвижный фон на welcome page. Не смог спозиционировать нормально. Леха помоги))

--- static/scripts/script_welcome.js
+++ static/scripts/script_welcome.js
@@ -1,10 +1,123 @@
 $(window).ready(function () {
     // showWelcome();
-    startAllFunctions();
-    setTimeout(function () {
-        startAllFunctions();
-    }, 200);
+    // startAllFunctions();
+    //
+    // setTimeout(function () {
+    //     startAllFunctions();
+    // }, 200);
 
+    particlesJS('particles-js',
+        {
+            "particles": {
+                "number": {
+                    "value": 158,
+                    "density": {
+                        "enable": true,
+                        "value_area": 3000
+                    }
+                },
+                "color": {
+                    "value": "#8b1deb"
+                },
+                "shape": {
+                    "type": "image",
+                    "stroke": {
+                        "width": 0,
+                        "color": "#37a930"
+                    },
+                    "polygon": {
+                        "nb_sides": 3
+                    },
+                    "image": {
+                        "src": "http://www.clker.com/cliparts/8/c/0/3/13683124941965114447graduation_cap-78-th.png",
+                        "width": 50,
+                        "height": 50
+                    }
+                },
+                "opacity": {
+                    "value": 1,
+                    "random": false,
+                    "anim": {
+                        "enable": false,
+                        "speed": 1,
+                        "opacity_min": 0.1,
+                        "sync": false
+                    }
+                },
+                "size": {
+                    "value": 80,
+                    "random": true,
+                    "anim": {
+                        "enable": false,
+                        "speed": 26.75531052375549,
+                        "size_min": 0.1,
+                        "sync": false
+                    }
+                },
+                "line_linked": {
+                    "enable": false,
+                    "distance": 0,
+                    "color": "#4aa6cc",
+                    "opacity": 1,
+                    "width": 12.327396437862724
+                },
+                "move": {
+                    "enable": true,
+                    "speed": 2,
+                    "direction": "top",
+                    "random": true,
+                    "straight": false,
+                    "out_mode": "out",
+                    "bounce": false,
+                    "attract": {
+                        "enable": false,
+                        "rotateX": 600,
+                        "rotateY": 1200
+                    }
+                }
+            },
+            "interactivity": {
+                "detect_on": "canvas",
+                "events": {
+                    "onhover": {
+                        "enable": false,
+                        "mode": "repulse"
+                    },
+                    "onclick": {
+                        "enable": false,
+                        "mode": "push"
+                    },
+                    "resize": true
+                },
+                "modes": {
+                    "grab": {
+                        "distance": 400,
+                        "line_linked": {
+                            "opacity": 1
+                        }
+                    },
+                    "bubble": {
+                        "distance": 400,
+                        "size": 40,
+                        "duration": 2,
+                        "opacity": 8,
+                        "speed": 3
+                    },
+                    "repulse": {
+                        "distance": 200,
+                        "duration": 0.4
+                    },
+                    "push": {
+                        "particles_nb": 4
+                    },
+                    "remove": {
+                        "particles_nb": 2
+                    }
+                }
+            },
+            "retina_detect": true
+        }
+    );
 });
 
 
@@ -104,9 +217,9 @@ $(window).on('resize', function () {
 });
 
 
-function startAllFunctions() {
-    setContentToCenter();
-}
+// function startAllFunctions() {
+//     setContentToCenter();
+// }
 //
 //
 // // когда высота страницы становится маленькой, выравнивает весь контент
