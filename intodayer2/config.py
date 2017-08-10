c2c231202d1269df0c6e2f83237ce3d5fcd724a7
Denis <borisovdenis-kb@yandex.ru>
2017-08-10 14:04:31 +0400

    Убрал config.py из игнора для Heroku

---
+++ intodayer2/config.py
@@ -0,0 +1,26 @@
+token = '264562282:AAF87uVKiLkp6DJQBIsqldUQiUFNZ2LyfOY'
+
+db_config_mysql = {
+    'NAME': 'intodayer',
+    'USER': 'denisko',
+    'PASSWORD': 'alexeyAndDenis1107*',
+    'HOST': '127.0.0.1',
+    'PORT': '3306',
+}
+
+db_config_pymysql = {
+    'db': 'intodayer',
+    'user': 'denisko',
+    'password': 'alexeyAndDenis1107*',
+    'host': '127.0.0.1',
+    'port': 3306,
+    'charset': 'utf8mb4'
+}
+
+PROJECT_EMAIL_PSWD = 'intodayer_uspeh777'
+PROJECT_EMAIL = 'intodayer@yandex.ru'
+
+BOT_PORT = 50007
+BOT_HOST = 'localhost'
+
+shelve_name = 'shelve.db'
