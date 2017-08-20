79a0c61bff45e4774d0e744ec4cddda25fb9be07
Denis <borisovdenis-kb@yandex.ru>
2017-08-20 10:59:37 +0400

    Поправил форматирование у триггеров

--- data_base/delete_unrelated_objects_trigger.sql
+++ data_base/delete_unrelated_objects_trigger.sql
@@ -6,15 +6,15 @@ CREATE TRIGGER del_unrelated_elements_after_update AFTER UPDATE ON plan_rows
 			DELETE FROM teachers WHERE id = OLD.teacher_id AND plan_id = OLD.plan_id;
 		END IF;
         
-        IF OLD.place_id NOT IN (SELECT place_id FROM plan_rows WHERE plan_id = OLD.plan_id) THEN
+    IF OLD.place_id NOT IN (SELECT place_id FROM plan_rows WHERE plan_id = OLD.plan_id) THEN
 			DELETE FROM places WHERE id = OLD.place_id AND plan_id = OLD.plan_id;
 		END IF;
         
-        IF OLD.subject_id NOT IN (SELECT subject_id FROM plan_rows WHERE plan_id = OLD.plan_id) THEN
+    IF OLD.subject_id NOT IN (SELECT subject_id FROM plan_rows WHERE plan_id = OLD.plan_id) THEN
 			DELETE FROM subjects WHERE id = OLD.subject_id AND plan_id = OLD.plan_id;
 		END IF;
         
-        IF OLD.time_id NOT IN (SELECT time_id FROM plan_rows WHERE plan_id = OLD.plan_id) THEN
+    IF OLD.time_id NOT IN (SELECT time_id FROM plan_rows WHERE plan_id = OLD.plan_id) THEN
 			DELETE FROM times WHERE id = OLD.time_id AND plan_id = OLD.plan_id;
 		END IF;
 	END|
@@ -29,15 +29,15 @@ CREATE TRIGGER del_unrelated_elements_after_delete AFTER DELETE ON plan_rows
 			DELETE FROM teachers WHERE id = OLD.teacher_id AND plan_id = OLD.plan_id;
 		END IF;
         
-        IF OLD.place_id NOT IN (SELECT place_id FROM plan_rows WHERE plan_id = OLD.plan_id) THEN
+    IF OLD.place_id NOT IN (SELECT place_id FROM plan_rows WHERE plan_id = OLD.plan_id) THEN
 			DELETE FROM places WHERE id = OLD.place_id AND plan_id = OLD.plan_id;
 		END IF;
         
-        IF OLD.subject_id NOT IN (SELECT subject_id FROM plan_rows WHERE plan_id = OLD.plan_id) THEN
+    IF OLD.subject_id NOT IN (SELECT subject_id FROM plan_rows WHERE plan_id = OLD.plan_id) THEN
 			DELETE FROM subjects WHERE id = OLD.subject_id AND plan_id = OLD.plan_id;
 		END IF;
         
-        IF OLD.time_id NOT IN (SELECT time_id FROM plan_rows WHERE plan_id = OLD.plan_id) THEN
+    IF OLD.time_id NOT IN (SELECT time_id FROM plan_rows WHERE plan_id = OLD.plan_id) THEN
 			DELETE FROM times WHERE id = OLD.time_id AND plan_id = OLD.plan_id;
 		END IF;
 	END|
